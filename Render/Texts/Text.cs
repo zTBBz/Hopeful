718d0fe1c4d2ec05bb67c266e9c8791649152ae9
zTBBz <warningstarini@yandex.ru>
2025-05-02 22:05:27 +0300

    🚧 Add base TextRenderSystem

---
+++ Render/Texts/Text.cs
@@ -0,0 +1,28 @@
+﻿using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Hopeful.Render.Texts;
+
+public struct Text : IComponent
+{
+    public string TextValue;
+    public string FontName;
+    public int FontSize;
+    public Vector2 Position;
+    public float Rotation;
+    public Color Color;
+
+    public Text(string text, string fontName, Vector2? position = null, Color? color = null, int fontSize = 14, float rotation = 0f)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(text);
+        ArgumentException.ThrowIfNullOrEmpty(fontName);
+
+        TextValue = text;
+        FontName = fontName;
+        FontSize = fontSize;
+        Position = (Vector2)(position == null ? Vector2.Zero : position);
+        Color = (Color)(color == null ? Color.White : color);
+        Rotation = rotation;
+    }
+}
---
+++ Render/Texts/TextRenderSystem.cs
@@ -0,0 +1,28 @@
+﻿using DryIocAttributes;
+using Friflo.Engine.ECS;
+using Friflo.Engine.ECS.Systems;
+using Hopeful.Asset;
+using Hopeful.Utilities;
+using System.ComponentModel.Composition;
+
+namespace Hopeful.Render.Texts;
+
+public class TextRenderSystem : QuerySystem<Text>
+{
+    [Import] // make another render system for mods
+    private readonly AssetManager _assets = null!;
+
+    [Import]
+    private readonly GlobalGraphics _graphics = null!;
+
+    protected override void OnUpdate()
+    {
+        Query.ForEachEntity((ref Text text, Entity entity) =>
+        {
+            if (_assets.TryGetFont(text.FontName, text.FontSize, out var font))
+            {
+                
+            }
+        });
+    }
+}
