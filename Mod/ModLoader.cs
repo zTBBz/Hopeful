7fe44277f997d015616ea93551f96b89831f5885
zTBBz <warningstarini@yandex.ru>
2025-05-02 22:05:01 +0300

    🚧 Add basic mods loading

---
+++ Mod/ModLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace Hopeful.Mod;
+
+public class ModLoader
+{
+    public bool TryLoadModAssembly(string path, out Assembly? assembly)
+    {
+        assembly = Assembly.LoadFrom(path);
+        return assembly != null;
+    }
+
+    public bool TryGetBaseMod(Assembly assembly, out BaseMod? mod)
+    {
+        var modType = assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(BaseMod)) && !t.IsAbstract).First();
+        mod = null;
+        try
+        {
+            mod = (BaseMod)Activator.CreateInstance(modType)!;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error mod creating {modType.Name}: {ex.Message}");
+        }
+
+        return mod != null;
+    }
+}
