811a5fb5a28df93b58f8efe6b2de7b664076c659
zTBBz <warningstarini@yandex.ru>
2025-05-02 22:06:39 +0300

    🚧 Add base Time

---
+++ Time.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Hopeful;
+
+public interface ITime
+{
+    float DeltaTime { get; }
+    float TotalTime { get; }
+}
+
+public readonly struct Time(GameTime gameTime) : ITime
+{
+    public float DeltaTime => (float)gameTime.ElapsedGameTime.TotalSeconds;
+    public float TotalTime => (float)gameTime.TotalGameTime.TotalSeconds;
+}
---
+++ Utilities/GameTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+
+namespace Hopeful.Utilities;
+
+public static class GameTimeExtensions
+{
+    public static UpdateTick ToUpdateTick(this ITime time)
+        => new(time.DeltaTime, time.TotalTime);
+
+    public static UpdateTick ToUpdateTick(this GameTime time)
+        => new(time.ElapsedGameTime.Seconds, time.TotalGameTime.Seconds);
+}
