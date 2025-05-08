34502dd203a1ac1e7463791b5421d9e034c7b0a0
zTBBz <warningstarini@yandex.ru>
2025-05-08 14:29:04 +0300

    🗑️ Remove excess code

--- Utilities/GameTimeExtensions.cs
+++ Utilities/GameTimeExtensions.cs
@@ -5,9 +5,12 @@ namespace Hopeful.Utilities;
 
 public static class GameTimeExtensions
 {
-    public static UpdateTick ToUpdateTick(this ITime time)
-        => new(time.DeltaTime, time.TotalTime);
-
     public static UpdateTick ToUpdateTick(this GameTime time)
         => new(time.ElapsedGameTime.Seconds, time.TotalGameTime.Seconds);
+
+    public static float DeltaTime(this GameTime time)
+        => (float)time.ElapsedGameTime.TotalSeconds;
+
+    public static float TotalTime(this GameTime time)
+        => (float)time.TotalGameTime.TotalSeconds;
 }
