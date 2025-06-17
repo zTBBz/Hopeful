705c0f3f35862f3e9850eac46b5f5620c88da103
zTBBz <warningstarini@yandex.ru>
2025-06-17 12:56:34 +0300

    ✨ Add base scripts

---
+++ Scripts/InjectScript.cs
@@ -0,0 +1,25 @@
+﻿using Friflo.Engine.ECS;
+
+namespace Hopeful.Scripts;
+
+public class InjectScript(Vault vault) : Script
+{
+    public readonly Vault Vault = vault;
+    public bool IsStarted { get; internal set; } = false;
+
+    public override void Start()
+    {
+        Store.OnScriptAdded += OnAdded;
+        Store.OnScriptRemoved += OnRemoved;
+        Vault.Inject(this);
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    protected virtual void OnAdded(ScriptChanged changed) { }
+    protected virtual void OnRemoved(ScriptChanged changed) { }
+}
---
+++ Scripts/ScriptSystem.cs
@@ -0,0 +1,30 @@
+﻿using Friflo.Engine.ECS.Systems;
+
+namespace Hopeful.Scripts;
+
+public sealed class ScriptSystem(Vault vault) : InjectBaseSystem(vault)
+{
+    protected override void OnUpdateGroup()
+    {
+        var scriptsSpan = Store.EntityScripts;
+
+        foreach (var scripts in scriptsSpan)
+        {
+            foreach (var script in scripts)
+            {
+                if (script is null) continue;
+
+                if (script is InjectScript injectScript)
+                {
+                    if (!injectScript.IsStarted)
+                    {
+                        injectScript.Start();
+                        injectScript.IsStarted = true;
+                    }
+                }
+
+                script.Update();
+            }
+        }
+    }
+}
